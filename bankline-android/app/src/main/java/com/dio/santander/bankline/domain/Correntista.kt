c5aedffbe0fb1932c1017cc3f20cf4ae4e687e11
Eduardo Watanabe Murakoshi <eduardowmu@yahoo.com.br>
2022-05-11 00:27:56 -0300

    Implementando Parcelable

--- bankline-android/app/src/main/java/com/dio/santander/bankline/domain/Correntista.kt
+++ bankline-android/app/src/main/java/com/dio/santander/bankline/domain/Correntista.kt
@@ -1,5 +1,10 @@
 package com.dio.santander.bankline.domain
 
+import android.os.Parcel
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Correntista(
     val id: Int
-)
+) : Parcelable
--- bankline-android/app/src/main/java/com/dio/santander/bankline/statement/BankStatementActivity.kt
+++ bankline-android/app/src/main/java/com/dio/santander/bankline/statement/BankStatementActivity.kt
@@ -2,6 +2,7 @@ package com.dio.santander.bankline.statement
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dio.santander.bankline.R
 
 class BankStatementActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
