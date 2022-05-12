78d606e606e592df98d021fb48e032157ca1fa0a
Eduardo Watanabe Murakoshi <eduardowmu@yahoo.com.br>
2022-05-11 23:59:04 -0300

    Alterando cores

--- bankline-android/app/src/main/java/com/dio/santander/bankline/statement/BankStatementActivity.kt
+++ bankline-android/app/src/main/java/com/dio/santander/bankline/statement/BankStatementActivity.kt
@@ -2,11 +2,29 @@ package com.dio.santander.bankline.statement
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.dio.santander.bankline.R
+import android.util.Log
+import com.dio.santander.bankline.databinding.ActivityBankStatementBinding
+import com.dio.santander.bankline.domain.Correntista
 
 class BankStatementActivity : AppCompatActivity() {
+    companion object {
+        const val EXTRA_ACCOUNT_HOLDER =
+            "com.dio.santander.bankline.statement.EXTRA_ACCOUNT_HOLDER"
+    }
+
+    private val binding by lazy {
+        ActivityBankStatementBinding.inflate(layoutInflater)
+    }
+
+    private val accountHolder by lazy {
+        intent.getParcelableExtra<Correntista>(
+            EXTRA_ACCOUNT_HOLDER) ?: throw IllegalArgumentException()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_bank_statement)
+        setContentView(binding.root)
+
+        Log.d("TESTE", "Chegou o ID: ${accountHolder.id}")
     }
 }
--- bankline-android/app/src/main/java/com/dio/santander/bankline/welcome/MainActivity.kt
+++ bankline-android/app/src/main/java/com/dio/santander/bankline/welcome/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.dio.santander.bankline.welcome
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.dio.santander.bankline.R
+import com.dio.santander.bankline.databinding.ActivityMainBinding
+import com.dio.santander.bankline.domain.Correntista
+import com.dio.santander.bankline.statement.BankStatementActivity
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.btContinue.setOnClickListener {
+            val accountHolderId = binding.etAccountHolderId.text.toString().toInt()
+            val accountHolder = Correntista(accountHolderId)
+
+            val intent = Intent(this, BankStatementActivity::class.java).apply {
+                putExtra(BankStatementActivity.EXTRA_ACCOUNT_HOLDER, accountHolder)
+            }
+            startActivity(intent)
+        }
     }
 }
