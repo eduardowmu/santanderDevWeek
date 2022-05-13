0eaf4e2b203e3e1226d8aad581f2e6c3ae6ed209
Eduardo Watanabe Murakoshi <eduardowmu@yahoo.com.br>
2022-05-12 22:46:37 -0300

    Listando dados de receita/despesa

--- bankline-android/app/src/main/java/com/dio/santander/bankline/statement/BankStatementActivity.kt
+++ bankline-android/app/src/main/java/com/dio/santander/bankline/statement/BankStatementActivity.kt
@@ -3,8 +3,12 @@ package com.dio.santander.bankline.statement
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.dio.santander.bankline.adapter.BankStatementAdapter
 import com.dio.santander.bankline.databinding.ActivityBankStatementBinding
 import com.dio.santander.bankline.domain.Correntista
+import com.dio.santander.bankline.domain.Movimentacao
+import com.dio.santander.bankline.domain.TipoMovimentacao
 
 class BankStatementActivity : AppCompatActivity() {
     companion object {
@@ -25,6 +29,25 @@ class BankStatementActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        Log.d("TESTE", "Chegou o ID: ${accountHolder.id}")
+        binding.rvBankStatement.layoutManager = LinearLayoutManager(this)
+
+        findBankStatement()
+    }
+
+    private fun findBankStatement() {
+        val dataSet = ArrayList<Movimentacao>()
+        dataSet.add(Movimentacao(1, "03/05/2022 09:24:55", "Salario",
+            10000.0, TipoMovimentacao.RECEITA, 1))
+
+        dataSet.add(Movimentacao(1, "03/05/2022 09:24:55", "Salario",
+            10000.0, TipoMovimentacao.RECEITA, 1))
+
+        dataSet.add(Movimentacao(1, "03/05/2022 09:24:55", "Salario",
+            10000.0, TipoMovimentacao.RECEITA, 1))
+
+        dataSet.add(Movimentacao(1, "03/05/2022 09:24:55", "Salario",
+            10000.0, TipoMovimentacao.RECEITA, 1))
+
+        binding.rvBankStatement.adapter = BankStatementAdapter(dataSet)
     }
 }
